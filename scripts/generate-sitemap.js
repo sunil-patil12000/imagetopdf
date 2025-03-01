410394f0ceb1966c4149314d9b0ad834066955b0
sunil patil <sunilpatil12000@gmail.com>
2025-03-01 19:42:05 +0530

    seo test

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,4 +1,6 @@
-const routes = ['/', '/blog', '/about'];
+import { writeFileSync } from 'fs';
+
+const routes = ['/'];
 const baseUrl = 'https://imgtopdf.sunilpatil.tech';
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
@@ -13,4 +15,4 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   `).join('')}
 </urlset>`;
 
-require('fs').writeFileSync('./public/sitemap.xml', sitemap);
+writeFileSync('./public/sitemap.xml', sitemap);
